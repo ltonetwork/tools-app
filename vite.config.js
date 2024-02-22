15c089eb1d5e933a611577d4caf99a5c8c125ce3
unknown <otobongfp@gmail.com>
2024-02-22 16:14:23 +0100

    add: deployment for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    BASE_URL: `"${process.env.VITE_BASE_URL}"`,
-  },
+  base: "/tools-app/",
+  // define: {
+  //   BASE_URL: `"${process.env.VITE_BASE_URL}"`,
+  // },
   plugins: [react()],
 });
