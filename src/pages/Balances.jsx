97d784f4c6cd60a7a42dc4d2c6c75e605928e1be
unknown <otobongfp@gmail.com>
2024-02-24 16:14:32 +0100

    removed redundant import

--- src/pages/Balances.jsx
+++ src/pages/Balances.jsx
@@ -6,7 +6,6 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import allAddresses from "../utils/data/allAddresses.json";
 import axios from "axios";
 import { BASE_URL } from "../utils/config";
 
