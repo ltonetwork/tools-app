8f0ae7aa02f53228b58043553dcdd9b5d498f4ff
unknown <otobongfp@gmail.com>
2024-02-28 14:27:35 +0100

    minor fixes

--- src/components/global/Loader.tsx
+++ src/components/global/Loader.tsx
@@ -5,8 +5,8 @@ const override: CSSProperties = {
   position: "fixed",
   top: "50%",
   left: "50%",
-  // transform: "translate(-50%, -50%)",
-  // zIndex: 9999,
+  transform: "translate(-50%, -50%)",
+  zIndex: 9999,
   display: "block",
 };
 
