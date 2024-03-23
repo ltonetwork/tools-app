fe02a00803499c1b4e0f86aef25af6f6673f81b3
otobongfp <otobongfp@gmail.com>
2024-03-23 08:22:37 +0100

    gh actions

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/tools-app/",
   define: {
     BASE_URL: `"${process.env.VITE_BASE_URL}"`,
   },
