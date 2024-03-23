0e30c8687a280d92d06d0e9cef0745e8f94f199a
otobongfp <otobongfp@gmail.com>
2024-03-23 08:30:53 +0100

    update to vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tools-app/",
+  base: "/",
   define: {
     BASE_URL: `"${process.env.VITE_BASE_URL}"`,
   },
